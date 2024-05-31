3fa16267e2da63d2d0ea3dbc561b5d2c4ac4432b
joshbnewton31080 <88124465+joshbnewton31080@users.noreply.github.com>
2024-05-30 21:24:13 -0400

    Update CreatingUnnecessaryObjectsServlet.java

--- src/main/java/org/t246osslab/easybuggy/performance/CreatingUnnecessaryObjectsServlet.java
+++ src/main/java/org/t246osslab/easybuggy/performance/CreatingUnnecessaryObjectsServlet.java
@@ -15,7 +15,7 @@ import org.t246osslab.easybuggy.core.servlets.AbstractServlet;
 @WebServlet(urlPatterns = { "/createobjects" })
 public class CreatingUnnecessaryObjectsServlet extends AbstractServlet {
 
-    @Override TESTJOSH
+    @Override
     protected void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
         Locale locale = req.getLocale();
         String strNumber = req.getParameter("number");
